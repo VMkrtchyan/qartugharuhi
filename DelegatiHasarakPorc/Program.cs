4764bbe2e2c122780d932f23562e2dd45213dae1
vardan mkrtchyan <boooooooom.vm@gmail.com>
2017-04-21 01:35:42 +0400

    Add project files.

---
+++ DelegatiHasarakPorc/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatiHasarakPorc
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Zangogh Shef = new Zangogh();
+            Shef.Name = "Harut";
+            Shef.Zang(metod1);
+            Shef.Tip = "Shef";
+
+            Zangogh Banvor = new Zangogh();
+            Banvor.Name = "Srap";
+            Banvor.Zang(metod2);
+            Banvor.Tip = "banvor";
+
+            Zangogh Partqater = new Zangogh();
+            Partqater.Name = "Smbo";
+            Partqater.Zang(metod3);
+            Partqater.Tip = "Partqater";
+            Console.Read();
+        }
+            
+
+
+       static Patasxan metod1 = QarugharuhuPat1;
+        static Patasxan metod2 = QarugharuhuPat2;
+        static Patasxan metod3 = QarugharuhuPat3;
+
+
+        static void QarugharuhuPat1 (string blablabla)
+        {
+        Console.WriteLine(blablabla);
+            Console.WriteLine("Lsum em shef jan");
+        }
+
+        static void QarugharuhuPat2(string blablabla )
+        {
+            Console.WriteLine(blablabla);
+            Console.WriteLine("El stex chzanges");
+        }
+
+        static void QarugharuhuPat3(string blablabla )
+        {
+            Console.WriteLine(blablabla);
+            Console.WriteLine("Tnoren@ dzez het shutov kkapnvi");
+        }
+    }
+}
---
+++ DelegatiHasarakPorc/Zangoghner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatiHasarakPorc
+{
+    public delegate void Patasxan (string massage);
+
+    class Zangogh
+    {
+        
+        public string Name { get; set; }
+
+        public string Tip { get; set; }
+
+        public void Zang( Patasxan metod )
+        {
+         metod(string.Format($"Barev, {Tip}"+$" {Name}n a")); 
+        }
+
+
+    }
+}
